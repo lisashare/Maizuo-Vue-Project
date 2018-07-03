3178566f8baa760912c4847aa2bb1529d4d2bb14
lisashare <lisa_zhu2012@foxmail.com>
2018-07-03 18:42:36 +0800

    header样式完成

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
-    
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  components:{},
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
