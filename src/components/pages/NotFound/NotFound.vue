017c48144aa9de71db14511dbb5967844b8b99fd
lisashare <lisa_zhu2012@foxmail.com>
2018-07-06 15:41:15 +0800

    404未知页面

--- src/components/pages/NotFound/NotFound.vue
+++ src/components/pages/NotFound/NotFound.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="not-found">
-        not-found
+    <div class="not-found page">
+        <div class="img-background page404">
+            <img width="100%" src="../../../../static/images/not-found.jpg" alt="">
+        </div>
+        <p>这个页面，神秘失踪了...</p>
+        <p>不如<router-link to="/home">返回卖座电影首页吧</router-link></p>
     </div>
 </template>
 <script>
@@ -8,6 +12,20 @@ export default {
     name:'NotFound'
 }
 </script>
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+.page404{
+    width:85%;
+    margin:45px auto 0;
+    
+}
+.not-found{
+    p{
+        font-size:16px;
+        color:#909090;
+        text-align: center;
+        a{
+            font-size:16px;
+        }
+    }  
+}
 </style>
