801f035ced650d208808703b3b3279bfc478b24d
lisashare <lisa_zhu2012@foxmail.com>
2018-07-11 15:49:24 +0800

    请求数据的提示信息交互

--- src/components/pages/Films/FilmsBox.vue
+++ src/components/pages/Films/FilmsBox.vue
@@ -8,26 +8,38 @@
     </div>
 </template>
 <script>
+import { Toast } from 'mint-ui'
 import FilmsItem from './FilmsItem'
+
 export default {
     name:"FilmsBox",
     props:['type'],
     components:{FilmsItem},
     data () {
         return {
-            films:[],
-            page:1,count:7
+            films:[],       //存放电影的数据
+            page:1,count:7, //页数 每页多少条
         }
     },
     methods:{
-        getFilms(){
-            let {page,count,type} = this;
-            console.log(type)
+        getFilms(){   //获取数据
+            
+            let {type,page,count} = this;
+
+            let instance = Toast({
+                'message':'正在加载',   //提示内容
+                // 'position':'bottom', //提示框位置，默认居中
+                'duration':-1,  //持续时间（毫秒），若为-1则不会自动关闭
+                'iconClass':'fa fa-cog fa-spin', //icon 图标的类名
+                // 'className':''  //Toast 的类名，可以为其添加样式
+            })
+            
             this.$http.get('/mz/v4/api/film/'+ type,{
                 params:[page,count]
             }).then(res=>{
-                console.log(res)
+                // console.log(res)
                 this.films = res.data.data.films;
+                instance.close();
             })
         }
     },
